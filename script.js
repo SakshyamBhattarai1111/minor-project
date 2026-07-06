1d9c66d07cb43b0c7e1dd8264356727718dafd85
Sakshyam <10sakshyam@gmail.com>
2026-07-06 21:36:39 +0545

    Updated project

--- script.js
+++ script.js
@@ -12,12 +12,32 @@ function login() {
         return false;
     }
 
-    if (username == "admin" && password == "admin123") {
-        alert("Welcome Admin");
+    if (username == "admin" && password == "1234") {
+        alert("Welcome, " + username);
+        window.location = "user.html";
     }
     else {
         alert("Login Successful");
     }
 
     return false;
+}
+function search(){
+var date = document.getElementById("date").value;
+var from= document.getElementById("from").value;
+var to = document.getElementById("to").value;
+if(date=="" || from=="" || to==""){
+    alert("Please fill all the fields");
+  
+}
+    else if(from==to){
+        alert("From and To locations cannot be same");
+    }
+    if (to=="Kathmandu" || from=="Pokhara" || to=="Pokhara" || from=="Kathmandu"){
+        alert("Search Successful");
+       window.location = "routes.html";
+    }
+}
+function search1(){
+alert("Please login first to search for routes");
 }
